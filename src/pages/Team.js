f8d4e14de25c711015c715e63fbf0f51e8df21ca
unknown <1481906095@qq.com>
2025-02-22 12:01:53 +0800

    降级到 React 18 并修复依赖

---
+++ src/pages/Team.js
@@ -0,0 +1,13 @@
+// src/pages/Team.js
+import React from 'react';
+
+const Team = () => {
+  return (
+    <div>
+      <h2>团队成员</h2>
+      <p>这里是团队介绍...</p>
+    </div>
+  );
+};
+
+export default Team;
